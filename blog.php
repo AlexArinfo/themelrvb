3acc50be9490e604e0ab4a30590cc286a4586848
fluoh <work@fluoh.fr>
2022-04-21 13:30:36 +0200

    dev page blog

---
+++ blog.php
@@ -0,0 +1,46 @@
+<?php get_header();
+//Template Name: Blog
+?>
+
+<section class="hero" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>');">
+</section>
+
+<main id="blog">
+
+
+<section class="container">
+<h2><?php the_title()?></h2>
+<?php
+$recentPosts = new WP_Query();
+$recentPosts->query('showposts=9');
+?>
+<?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+<article>
+<div class="imgarticle">
+<?php if (has_post_thumbnail()) : ?>
+    <?php the_post_thumbnail('blogpage_thumb'); ?>
+    <?php endif; ?>
+
+    </div>
+    
+    <div class="textearticle">
+    <span class="date">
+    <?php the_time('l, jS F, Y'); ?>
+    </span>
+    <h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+ 
+    <?php the_excerpt(); ?>
+    <span><?php the_category(); ?></span>
+    <span><?php the_author();?></span>
+    </div>
+    </article>
+    <?php endwhile; ?>
+    
+    </section>
+    </main>
+    <?php get_footer(); ?>
+    
+    
+    
+    
+    
