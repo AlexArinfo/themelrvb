73d7e54e2b9119649dc3dd177c64c51bf7dcc0c7
fluoh <work@fluoh.fr>
2022-04-21 15:18:40 +0200

    ajout wphead

--- header.php
+++ header.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-<meta charset="UTF-8" />
-
+<head <?php language_attributes(); ?>>
+<!--    <meta charset="<?php bloginfo('charset'); ?>">-->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title><?php the_title(); ?> </title>
+<?php wp_head(); ?>
 </head>
+
 <body>
-<div class="bdp-container">
-<div class="bdp" id="barre"></div>
-</div>
-<header>
+  <header>
 <?php
 wp_nav_menu(array(
   'menu' => 'Menu principal',
