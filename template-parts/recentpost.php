044eafa123db11c17ad8168b6e1f22d0bcee6f9d
fluoh <work@fluoh.fr>
2022-04-27 09:27:59 +0200

    singleok

--- template-parts/recentpost.php
+++ template-parts/recentpost.php
@@ -1,26 +1,31 @@
 <div>
-  <h2>Articles Récents</h2>
+<h2>Articles Récents</h2>
 <?php
 // Define our WP Query Parameters
 $the_query = new WP_Query( 'posts_per_page=5' ); ?>
-  
+
 <?php
 // Start our WP Query
 while ($the_query -> have_posts()) : $the_query -> the_post();
 // Display the Post Title with Hyperlink
 ?>
-  
-  <article>
- 
-<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-<?php the_post_thumbnail('recentpost-thumb'); ?>
-<?php
-// Display the Post Excerpt
-the_excerpt(__('(more…)')); ?>
-</article>
-  
 
-  
+<article>
+
+
+<?php the_post_thumbnail('recentpost-thumb'); ?>
+<div class="sideinfos">
+<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+<div class="sideinfosbis">
+<span class="aut"><?php the_author();?></span>
+<span class="cat"><?php the_category(); ?></span>
+</div>
+</div>
+
+</article>
+
+
+
 <?php
 // Repeat the process and reset once it hits the limit
 endwhile;
